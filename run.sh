b3e67341de96386e02cc1f2d832d217fafbaaca6
khai96_ <hvksmr1996@gmail.com>
2020-05-08 12:05:45 +0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+export INPUT_VERSION=4.11.1
+export INPUT_DEST=pnpm.temp
+export INPUT_BIN_DEST=pnpm.temp/.bin
+export INPUT_REGISTRY=https://registry.npmjs.com
+make dist/index.js || exit $?
+exec node dist/index.js

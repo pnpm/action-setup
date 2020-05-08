429bc133fdca6b9fd263a0a5f46c7de4dec472b9
khai96_ <hvksmr1996@gmail.com>
2020-05-08 14:16:01 +0700

    Set HOME in run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #! /bin/sh
+# shellcheck disable=SC2155,SC2088
+export HOME="$(pwd)"
 export INPUT_VERSION=4.11.1
-export INPUT_DEST=pnpm.temp
-export INPUT_BIN_DEST=pnpm.temp/.bin
+export INPUT_DEST='~/pnpm.temp'
+export INPUT_BIN_DEST='~/pnpm.temp/.bin'
 export INPUT_REGISTRY=https://registry.npmjs.com
 exec node dist/index.js

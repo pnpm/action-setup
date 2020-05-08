fe4a915fbf74666ee9db717687ae6f3246960355
khai96_ <hvksmr1996@gmail.com>
2020-05-08 14:26:14 +0700

    Return expanded paths

--- src/outputs/index.ts
+++ src/outputs/index.ts
@@ -1,8 +1,10 @@
-import { addPath } from '@actions/core'
+import { setOutput, addPath } from '@actions/core'
 import { Inputs } from '../inputs'
 
 export function setOutputs(inputs: Inputs) {
   addPath(inputs.binDest)
+  setOutput('dest', inputs.dest)
+  setOutput('bin_dest', inputs.binDest)
 }
 
 export default setOutputs

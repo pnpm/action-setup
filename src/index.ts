291e58ad85d11f68c6b31db09a4418890625f8b6
khai96_ <hvksmr1996@gmail.com>
2020-05-09 21:02:32 +0700

    Enable post action

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import { setFailed } from '@actions/core'
+import { setFailed, saveState, getState } from '@actions/core'
 import getInputs from './inputs'
 import setOutputs from './outputs'
 import installPnpm from './install-pnpm'
 import pnpmInstall from './pnpm-install'
 
 async function main() {
+  const isPost = getState('is_post')
+  if (isPost === 'true') return
+  saveState('is_post', 'true')
   const inputs = getInputs()
   await installPnpm(inputs)
   console.log('Installation Completed!')

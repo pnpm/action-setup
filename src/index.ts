d2316507fdc206ec21aef058b2cbcb47c02391c8
khai96_ <hvksmr1996@gmail.com>
2020-05-09 20:47:42 +0700

    Inspect isPost

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-import { setFailed } from '@actions/core'
+import { setFailed, getState, debug } from '@actions/core'
 import getInputs from './inputs'
 import setOutputs from './outputs'
 import installPnpm from './install-pnpm'
 import pnpmInstall from './pnpm-install'
 
 async function main() {
+  const isPost = getState('isPost')
+  debug(JSON.stringify(isPost))
+  if (isPost) {
+    return
+  }
   const inputs = getInputs()
   await installPnpm(inputs)
   console.log('Installation Completed!')

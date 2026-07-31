13bdc8a6435b891c48b1e133d614aff66356c074
SukkaW <isukkaw@gmail.com>
2026-08-01 01:41:23 +0800

    refactor: introduce restore keys for cache

--- src/cache-restore/run.ts
+++ src/cache-restore/run.ts
@@ -18,7 +18,13 @@ export async function runRestoreCache(inputs: Inputs) {
   debug(`Primary key is ${primaryKey}`)
   saveState('cache_primary_key', primaryKey)
 
-  let cacheKey = await restoreCache([cachePath], primaryKey)
+  // We don't need to download everything again if only one dependency changed
+  // We can still re-use previous store to cache the rest of the unchanged dependencies
+  const restoreKeys = [
+    `pnpm-cache-${process.env.RUNNER_OS}-${os.arch()}-`
+  ];
+
+  let cacheKey = await restoreCache([cachePath], primaryKey, restoreKeys)
 
   setOutput('cache-hit', Boolean(cacheKey))
 
